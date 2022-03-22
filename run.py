cdc64f9550836cca018bee54c96d5781a0ceba0e
simeseg <48680907+simeseg@users.noreply.github.com>
2022-03-22 19:18:54 +0900

    Update run.py

--- run.py
+++ run.py
@@ -7,14 +7,12 @@ Created on Thu Mar 10 12:20:15 2022
 
 import numpy as np
 import open3d as o3d
-
 import utils
 from icp_GN import icp_GN
 from icp_LM import icp_LM
 
-
-model = o3d.io.read_point_cloud("./BunnyData/bun000_UnStructured.pcd") #("./BunnyData/bun000_UnStructured.pcd") ("./BunnyData/bun000_UnStructured.pcd")("valve.pcd")###
-scene = o3d.io.read_point_cloud("./BunnyData/bun045_UnStructured.pcd") #("./bolt_model/shcb_m10x40_corrected.pcd")#("boltclip0.pcd")#("valve_2.pcd")##("./bolt_model/shcb_m10x40_corrected.pcd")#("boltpcd2.pcd")#("./BunnyData/bun000_UnStructured.pcd")#
+model = o3d.io.read_point_cloud("./BunnyData/bun000_UnStructured.pcd") 
+scene = o3d.io.read_point_cloud("./BunnyData/bun045_UnStructured.pcd")
 
 model = utils.NanInfScale(model, 1000)
 scene = utils.NanInfScale(scene, 1000)
@@ -25,5 +23,4 @@ scene.points = o3d.utility.Vector3dVector(utils.Rot3D(np.asarray(scene.points), 
 
 #args: grid_size, distance threshold, iterations, error mode, model, scene
 icp = icp_GN(4, 1, 20, "plane", False, model, scene)
-
 icp = icp_LM(4, 1, 20, "plane", True, model, scene)
